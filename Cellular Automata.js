49dfa39097892750154ec3dd32036bb5614d3c39
Computedduke586 <jarviekyle@gmail.com>
2025-10-15 19:02:10 -0600

    Update Cellular Automata.js

--- Cellular Automata.js
+++ Cellular Automata.js
@@ -69,7 +69,7 @@ function setup() {
   input0.position(198, 40);
   input0.input(() => initiated = false);
   
-  text("Loop", 155, 100);
+  text("Loop", 155, 110);
   checkbox = createCheckbox();
   checkbox.position(135, 86);
 }
@@ -169,4 +169,5 @@ function NumberingSystem(int1, int2, int3, int4, int5, int6, int7, int8){
     }
   }
   colour = nextColour.slice();  
+
 }
